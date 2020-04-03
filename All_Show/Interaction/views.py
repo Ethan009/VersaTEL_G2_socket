855ab87cb8f12ffd8bd614f4d964af7b90ba1a65
Paul.Wen <Paul.Wen@feixitek.com>
2020-04-03 16:35:19 +0800

    paul

--- All_Show/Interaction/views.py
+++ All_Show/Interaction/views.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, jsonify, render_template, request, make_response, Blueprint
 from All_Show.Interaction import interaction_blue
-
+import VersaTELSocket as vst
 import json
 from flask_cors import *
 
@@ -36,11 +36,26 @@ def send_message():
 @interaction_blue.route('/LINSTOR_message', methods=['GET'])
 def LINSTOR_message():
     Node = ['Node_Name', 'IP', 'Node_Type']
-    Storage_pool = ['SP_Name', 'Node_One', 'vg_lv']
+    Storage_pool = ['SP_Name', 'Node_One_Text', 'lvm_name','lv_Text']
     
     if request.method == 'GET':
         data_all = request.args.items()
         print("data_all:", data_all)
-        return "test"
+    for i in data_all:
+            data_one_dict = {i[0]:i[1]}
+            data_dict.update(data_one_dict)
+    for i in  data_dict:
+        if i in Node:
+            break
+        if i in Storage_pool:
+            print(data_dict.values()[0])
+            print(data_dict.values()[1])
+            print(data_dict.values()[2])
+            print(data_dict.values()[3])
+            
+            #str_cmd = "python3 vtel.py stor sp c %s -n %s %s %s"%(sp,node,'-lvm','vg1')
+            #sessage = vst.conn(str_cmd)
+            
+        return sessage
     else:
         return "test"
